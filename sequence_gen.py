2cdf32a641273736c356d9f9d097c8f0b685ae86
rajesh  jha <65206807+rajesh-n-j@users.noreply.github.com>
2021-02-18 01:42:16 +0530

    Update sequence_gen.py
    changed dictionary name

--- sequence_gen.py
+++ sequence_gen.py
@@ -1,10 +1,10 @@
 def dynamic_sequence_template_gen(number):
-    arr_1 = {"0":"A","1": "B", "2":"C", "3":"D","4":"E","5":"F","6":"G","7":"H","8":"I","9":"J"}
+    static_template = {"0":"A","1": "B", "2":"C", "3":"D","4":"E","5":"F","6":"G","7":"H","8":"I","9":"J"}
     created_temp = {}
     for idx in range(1,number+1):
         created_number_temp = ""
         for idn in str(idx):
-            created_number_temp += arr_1[idn]
+            created_number_temp += static_template[idn]
         created_temp[idx]= created_number_temp   
     return created_temp
 
